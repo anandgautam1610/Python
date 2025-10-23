16872882fc5b4e7c1afe4290c09ae0cdf0017c18
Anand Gautam <anandgautam1610@gmail.com>
2025-10-23 20:05:31 +0530

    Add files via upload

---
+++ Pythontask1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+Addition = a + b # Addition of two numbers
+Subtraction = a - b # Subtraction of two numbers
+Multiplication = a * b # Multiplication of two numbers
+Division = a / b # Division of two numbers
+
+print("Addition:", Addition)
+print("Subtraction:", Subtraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
